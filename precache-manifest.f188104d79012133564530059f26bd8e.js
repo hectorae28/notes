c8a21d10c37fe3d20a4bcd0fadc86c576e73eed1
hectorae28 <hectorae28@gmail.com>
2019-09-17 23:45:09 -0400

    Updates

---
+++ precache-manifest.f188104d79012133564530059f26bd8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c8445e39095c39097f",
+    "url": "/notes/static/css/main.856e6b15.chunk.css"
+  },
+  {
+    "revision": "35c8445e39095c39097f",
+    "url": "/notes/static/js/main.35c8445e.chunk.js"
+  },
+  {
+    "revision": "fd7814485752c45f6c50",
+    "url": "/notes/static/js/1.fd781448.chunk.js"
+  },
+  {
+    "revision": "1291e1b612bc298497c9",
+    "url": "/notes/static/js/runtime~main.1291e1b6.js"
+  },
+  {
+    "revision": "2dd58ba3ece506f91c6e21d69be18689",
+    "url": "/notes/index.html"
+  }
+];
